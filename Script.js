2a25dcfd209eeecd723a2e2308e6b969bfafe2ce
nash-omy <walumbenaomi8@gmail.com>
2025-05-13 10:01:51 +0300

    first commit

---
+++ Script.js
@@ -0,0 +1,26 @@
+body {
+  font-family: Arial, sans-serif;
+  padding: 20px;
+}
+
+#hoverBox {
+  background-color: lightgray;
+  padding: 10px;
+  width: 150px;
+  margin-top: 10px;
+}
+
+.tabContent {
+  margin-top: 10px;
+  border: 1px solid #ccc;
+  padding: 10px;
+}
+
+button {
+  margin: 5px;
+}
+
+#colorBtn {
+  padding: 10px;
+  transition: background-color 0.5s ease;
+}
